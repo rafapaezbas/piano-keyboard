0fc06112ec6a4e90064c5dd722f4c0a94ebd77ee
rafapaezbas <rpaezbas@rpaezbas.com>
2022-12-09 20:30:16 +0100

    added note event and noe highlight

--- index.js
+++ index.js
@@ -4,39 +4,45 @@ window.customElements.define('piano-keyboard', class PianoKeyboard extends HTMLE
   constructor () {
     super()
     this.root = this.attachShadow({ mode: 'open' })
-
     this.canvas = document.createElement('canvas')
     this.canvas.setAttribute('width', this.getAttribute('width') || 600)
-    this.canvas.setAttribute('height', this.getAttribute('width') || 100)
-
+    this.canvas.setAttribute('height', this.getAttribute('height') || 100)
     this.root.appendChild(this.canvas)
-
     this.ctx = this.canvas.getContext('2d')
     this.width = this.canvas.width
     this.height = this.canvas.height
-
-    this.octaves = 3
+    this.octaves = this.getAttribute('octaves') || 3
     this.whiteKeyWidth = (this.width / this.octaves) / 7
-    this.blackKeyWidth = this.whiteKeyWidth - this.whiteKeyWidth / 3
+    this.blackKeyWidth = (this.whiteKeyWidth - this.whiteKeyWidth / 3) - 1
     this.blackKeyHeight = this.height / 2
     this.blackKeysOffset = this.whiteKeyWidth / 2 + (this.whiteKeyWidth / 6)
+    this.notes = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
+    this.pressedKeys = []
+  }
 
-    // Draw white keys
+  draw (pressedKeys = []) {
+    this.drawWhiteKeys(pressedKeys)
+    this.drawBlackKeys(pressedKeys)
+  }
 
+  drawWhiteKeys (pressedKeys = []) {
     for (let i = 0; i < this.octaves * 7; i++) {
+      const note = this.notes[i % 7] + Math.floor(i / 7)
       this.ctx.lineWidth = 1
       this.drawBorder(this.whiteKeyWidth * i, 0, this.whiteKeyWidth, this.height, 1)
-      this.ctx.fillStyle = 'white'
+      this.ctx.fillStyle = pressedKeys.indexOf(note) !== -1 ? 'red' : 'white'
       this.ctx.fillRect(this.whiteKeyWidth * i, 0, this.whiteKeyWidth, this.height)
     }
+  }
 
-    // Draw black keys
-
+  drawBlackKeys (pressedKeys = []) {
     let i = 0
     let j = 0
     while (i < this.octaves * 5) {
       if (j % 7 !== 2 && j % 7 !== 6) {
-        this.ctx.fillStyle = 'black'
+        const note = this.notes[j % 7] + '#' + Math.floor(j / 7)
+        this.drawBorder((this.whiteKeyWidth * j) + this.blackKeysOffset, 0, this.blackKeyWidth, this.blackKeyHeight, 1)
+        this.ctx.fillStyle = pressedKeys.indexOf(note) !== -1 ? 'red' : 'black'
         this.ctx.fillRect((this.whiteKeyWidth * j) + this.blackKeysOffset, 0, this.blackKeyWidth, this.blackKeyHeight)
         i++
       }
@@ -53,28 +59,37 @@ window.customElements.define('piano-keyboard', class PianoKeyboard extends HTMLE
     const rect = this.canvas.getBoundingClientRect()
     const x = event.clientX - rect.left
     const y = event.clientY - rect.top
-    console.log('x: ' + x + ' y: ' + y)
-    console.log('note', this.getNote(x, y))
+    return { x, y }
   }
 
-  getNote (x, y) {
-    const notes = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
+  getNote ({ x, y }) {
     const position = Math.floor(x / this.whiteKeyWidth)
-    const note = notes[position % 7]
+    const note = this.notes[position % 7]
+    const octave = Math.floor(x / (this.whiteKeyWidth * 7))
 
     if (y < this.blackKeyHeight && (x % this.whiteKeyWidth) > this.blackKeysOffset && note !== 'e' && note !== 'b') {
-      return note + '#'
+      return note + '#' + octave
     }
     if (y < this.blackKeyHeight && (x % this.whiteKeyWidth) < this.blackKeyWidth / 2 && note !== 'f' && note !== 'c') {
-      return notes[(position - 1) % 7] + '#'
+      return this.notes[(position - 1) % 7] + '#' + octave
     }
 
-    return note
+    return note + octave
   }
 
   async connectedCallback () {
+    this.draw()
+
     this.canvas.addEventListener('mousedown', (e) => {
-      this.getCursorPosition(this.canvas, e)
+      const note = this.getNote(this.getCursorPosition(this.canvas, e))
+      this.pressedKeys.push(note)
+      this.draw(this.pressedKeys)
+      this.dispatchEvent(new CustomEvent('key-pressed', { detail: { note }, bubbles: true, composed: true }))
+    })
+
+    this.canvas.addEventListener('mouseup', (e) => {
+      this.pressedKeys = []
+      this.draw(this.pressedKeys)
     })
   }
 })
